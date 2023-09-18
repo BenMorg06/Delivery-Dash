f8aa7ff5a1047a95e9bd905cf53957af444b69e7
bwifo <ben.morg06@gmail.com>
2023-09-18 09:53:30 +0100

    Add map onto pygame window

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame, os, math, sys
 # Setup Pygame Window
 WIDTH, HEIGHT = 1244, 700
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+Track = pygame.transform.rotozoom(pygame.image.load(os.path.join("Assets", "Picture 1.png")), 0, 2.4)
 
 # Main loop
 run = True
@@ -12,5 +13,6 @@ while run:
         if event.type == pygame.QUIT:
             quit()
     SCREEN.fill((0,200,0))
+    SCREEN.blit(Track, (-20,-20))
 
     pygame.display.update()
