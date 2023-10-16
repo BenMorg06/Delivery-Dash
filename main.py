a7d53978fce0bed151a6265c39fa96c3e064abb1
bwifo <ben.morg06@gmail.com>
2023-10-16 10:51:18 +0100

    Map side collisions

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ TRACK = pygame.image.load(os.path.join("Assets","city map 1.png"))
 
 # Define matrix
 
+
+# Track collisions
+# I will no longer be using a mask for the track collisions because it will be more efficient for time and ease of code.
+# These collisions will be added once the car is created
+
 # Main loop
 run = True
 while run:
