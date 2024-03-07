9e4c78daab2bbd6e3d497741d2ba4b140ed35522
bwifo <ben.morg06@gmail.com>
2024-03-07 09:58:28 +0000

    highscore values displayed

--- highscore.py
+++ highscore.py
@@ -21,7 +21,24 @@ class Highscore():
 
     # Get Highscore #
     def get_highscore(self):
-        pass
+        f = open('highscores.csv','r')
+        scores = f.readlines()
+        user_scores = {}
+        while len(user_scores) != len(scores):
+            highscore = 0
+            user = ''
+            for i in scores:
+                if highscore < int(i.split(',')[1]) and i.split(',')[0] not in user_scores:
+                    highscore = int(i.split(',')[1])
+                    user = i.split(',')[0]
+                else: pass
+            user_scores[user] = int(highscore)
+        self.first_user = FONT.render(f"{list(user_scores.keys())[0]} : {list(user_scores.values())[0]}", False, '#2D6A4F') #
+        self.second_user = FONT.render(f"{list(user_scores.keys())[1]} : {list(user_scores.values())[1]}", False, '#2D6A4F') #
+        self.third_user = FONT.render(f"{list(user_scores.keys())[2]} : {list(user_scores.values())[2]}", False, '#2D6A4F') #
+        self.first_rect = self.first_user.get_rect(midleft = (WIDTH//2 -200, 300))
+        self.second_rect = self.second_user.get_rect(midleft = (WIDTH//2 -200, 350))
+        self.third_rect = self.third_user.get_rect(midleft = (WIDTH//2 -200, 400))
 
     # RUN #
     def run(self):
@@ -38,9 +55,13 @@ class Highscore():
             # draw the title
             self.screen.blit(self.title, self.title_rect)
             self.screen.blit(self.highscore, self.highscore_rect)
+            self.screen.blit(self.first_user, self.first_rect)
+            self.screen.blit(self.second_user, self.second_rect)
+            self.screen.blit(self.third_user, self.third_rect)
             
             pygame.display.flip()
             CLOCK.tick(FPS)
 
 test = Highscore()
+test.get_highscore()
 test.run()
--- test.py
+++ test.py
@@ -1,2 +1,4 @@
 #f = open('current_user.txt', 'r')
 #print(f.read())
+user_scores = {'sores':23,'score':11}
+print(list(user_scores.keys())[1])
